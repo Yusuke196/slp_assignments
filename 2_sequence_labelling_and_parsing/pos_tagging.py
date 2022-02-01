f219a0daf7098311316745ea9b7e77d370157403
Yusuke196 <mondo.di.3301@gmail.com>
2022-02-01 19:56:39 +0900

    biterbiアルゴリズムのforwardを実装

--- 2_sequence_labelling_and_parsing/pos_tagging.py
+++ 2_sequence_labelling_and_parsing/pos_tagging.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 from pprint import pprint
 
 
@@ -72,7 +73,7 @@ def _calc_prob(cnt: dict[str, dict], transmission=True, n=None) -> dict[str, dic
         if transmission:
             prob[k] = {token: cnt / total for token, cnt in dct.items()}
         else:
-            # interpolationによるsmoothingを実行
+            # emission_probについては、interpolationによるsmoothingを実行
             prob[k] = {token: lambd * cnt / total + (1 - lambd) / n for token, cnt in dct.items()}
             prob[k].update({'<UNK>': (1 - lambd) / n})
     return prob
@@ -81,22 +82,30 @@ def _calc_prob(cnt: dict[str, dict], transmission=True, n=None) -> dict[str, dic
 def predict(sent: list[tuple], probs: list[dict]):
     tra_prob, emi_prob, utag_prob = probs
     uniq_tag = set(utag_prob.keys())
-    print(f'{len(uniq_tag) = }')
-    viterbi = [[0] * len(uniq_tag) for _ in range(len(sent))]
+    best_score = []
 
-    for t in range(len(sent)):
-        for tag_i, tag in enumerate(uniq_tag):
-            if t == 0:
-                viterbi[t][tag_i] = emi_prob[tag].get(sent[t][0], 0)
+    for w_i in range(len(sent)):
+        best_score.append({key: -np.inf for key in uniq_tag})
+        for tag in uniq_tag:
+            if w_i == 0:
+                best_score[w_i][tag] = _log(emi_prob[tag].get(sent[w_i][0], 0))
             else:
-                probs = []
-                for prev_tag in (uniq_tag - {'</s>'}):
+                lprobs = []
+                for prev_tag in uniq_tag - {'</s>'}:
                     # P(w|t)P(t|t-1)
-                    ep = emi_prob[tag].get(sent[t][0], emi_prob[tag]['<UNK>'])
+                    prev_score = best_score[w_i - 1][prev_tag]
                     tp = tra_prob[prev_tag].get(tag, 0)
-                    probs.append(ep * tp)
-                viterbi[t][tag_i] = max(probs)
-    pprint(viterbi[:2])
+                    lprobs.append(prev_score + _log(tp))
+                ep = emi_prob[tag].get(sent[w_i][0], emi_prob[tag]['<UNK>'])
+                best_score[w_i][tag] = max(lprobs) * ep
+    # pprint(best_score[:2])
+
+
+def _log(num: float) -> float:
+    if num == 0:
+        return -np.inf
+    else:
+        return np.log(num)
 
 
 if __name__ == '__main__':
